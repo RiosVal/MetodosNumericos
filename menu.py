7647da226657371af7fbac756fa440ef853d3e36
RiosVal <riosh.valeria@gmail.com>
2023-08-10 09:40:40 -0500

    Agrego menú sin funcionalidades

---
+++ menu.py
@@ -0,0 +1,36 @@
+from calculadora import *
+
+def menu():
+    print("-----------------------------------------------------------------")
+    print("                           Calculadora                           ")
+    print("-----------------------------------------------------------------")
+    print("1. Calcular la derivada central de una funcion")
+    print("2. Calcular la derivada hacia adelante de una funcion")
+    print("3. Calcular la derivada hacia atras de una funcion")
+    print("4. Calcular la raiz de una función con el método del punto fijo")
+    print("5. Calcular la raiz de una función con el método de biseccion")
+    print("6. Calcular la raiz de una función con el método de falsa posición")
+    print("7. Calcular la raiz de una función con el método de nexton raphson")
+    opcion = int(input("Escoge la opcion de tu preferencia"))
+    return opcion
+
+def opcion1():
+    funcion = input("Ingresa la función a calcular")
+
+def opcion2():
+    funcion = input("Ingresa la función a calcular")
+
+def opcion3():
+    funcion = input("Ingresa la función a calcular")
+
+def opcion4():
+    funcion = input("Ingresa la función a calcular")
+
+def opcion5():
+    funcion = input("Ingresa la función a calcular")
+
+def opcion6():
+    funcion = input("Ingresa la función a calcular")
+
+def opcion7():
+    funcion = input("Ingresa la función a calcular")
