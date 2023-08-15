a7650c9b75004e2b78252459552bb28415d48a23
Danigs2301 <117946884+Danigs2301@users.noreply.github.com>
2023-08-15 11:08:31 -0500

    cambio Daniel
    Haciendo menú y validación de datos
    Debo hacer la petición de la función al usuario para que sea funcional y ejecutable

--- menu.py
+++ menu.py
@@ -1,31 +1,39 @@
 from calculadora import *
 
-def pedirFuncion()
+def pedirFuncion():
     funcion = input("Ingresa la función a calcular: ")
     return funcion
 
 def menu():
-    print("-----------------------------------------------------------------")
-    print("                           Calculadora                           ")
-    print("-----------------------------------------------------------------")
-    print("1. Calcular la derivada central de una funcion")
-    print("2. Calcular la derivada hacia adelante de una funcion")
-    print("3. Calcular la derivada hacia atras de una funcion")
-    print("4. Calcular la raiz de una función con el método del punto fijo")
-    print("5. Calcular la raiz de una función con el método de biseccion")
-    print("6. Calcular la raiz de una función con el método de falsa posición")
-    print("7. Calcular la raiz de una función con el método de nexton raphson")
-    opcion = int(input("Escoge la opcion de tu preferencia"))
+    bandera = True
+    
+    while (bandera):
+        bandera =False 
+        print("-----------------------------------------------------------------")
+        print("                           Calculadora                           ")
+        print("-----------------------------------------------------------------")
+        print("1. Calcular la derivada central de una funcion")
+        print("2. Calcular la derivada hacia adelante de una funcion")
+        print("3. Calcular la derivada hacia atras de una funcion")
+        print("4. Calcular la raiz de una función con el método del punto fijo")
+        print("5. Calcular la raiz de una función con el método de biseccion")
+        print("6. Calcular la raiz de una función con el método de falsa posición")
+        print("7. Calcular la raiz de una función con el método de nexton raphson")
+        opcion = int(input("Escoge la opcion de tu preferencia"))
+        if opcion < 0 or opcion > 7:
+            print("Ingrese un número válido")
+            bandera =True
+        
     fun = pedirFuncion()
-    switch (opcion) {
-        case 1:
-        case 2:
-        case 3:
-        case 4:
-        case 5:
-        case 6:
-        case 7:        
-    }
+    if opcion== 1:
+    elif opcion== 2:
+    elif opcion== 3:
+    elif opcion== 4:
+    elif opcion== 5:
+    elif opcion== 6:
+    elif opcion== 7:
+    
+        
     
 
 
