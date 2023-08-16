a0c0ba50d2eeba0f894b1881ba9127be8291b24c
RiosVal <riosh.valeria@gmail.com>
2023-08-16 07:24:03 -0500

    Agrego y modifico metodo de biseccion

--- tarea_clase4.py
+++ tarea_clase4.py
@@ -15,9 +15,6 @@ def metodoFalsaPosicion(f, x0, x1, tol):
             return None
 
 
-        #if error <= tol:
-            #break
-
         if f(x0) * f(pos) < 0:
             x1 = pos
             anterior = x1
@@ -25,6 +22,31 @@ def metodoFalsaPosicion(f, x0, x1, tol):
             x0 = pos
             anterior = x0
     return (i, pos, error)
+
+
+def metodoBiseccion(f, x0, x1, tol):
+    anterior = x0
+    for i in range(50):
+        if f(x0) * f(x1) < 0:
+            medio = (x0 + x1)/2
+        else:
+            return None
+            #print('La función no tiene cero en ese intervalo')
+
+        error = abs((medio - anterior) / medio)
+
+        if error <= tol:
+            break
+
+        if f(x0) * f(medio) < 0:
+            x1 = medio
+            anterior = x1
+        else:
+            x0 = medio
+            anterior = x0
+
+    return (i, medio, error)
+    #print('{:<6} {:<22} {:<20}'.format(i, medio, error))   
 
 def cos(x, n=50):
     suma = 0
@@ -40,7 +62,7 @@ superior = 0
 
 while superior < limite:
     superior = inferior + 0.2
-    raiz = metodoFalsaPosicion(cos, inferior, superior, 0.0000001)
+    raiz = metodoBiseccion(cos, inferior, superior, 0.0000001)
     if raiz != None:
         raices.append([inferior, superior])
     inferior = superior
