3712e1578f5caba6b706769b5da8a9c603b5f603
Danigs2301 <117946884+Danigs2301@users.noreply.github.com>
2023-08-27 21:19:13 -0500

    CalculadoraFinalSegundoCorte

--- calculadora.py
+++ calculadora.py
@@ -1,6 +1,5 @@
 import math
 from funcsMenu import usarFuncion
-import sympy as sp
 
 
 def funcionLn(x):       #prueba de derivadas
@@ -27,66 +26,110 @@ def derivadaAdelante(f, x, h):
 def metodoPuntoFijo(g, x0, tol):
     xold = x0
     for i in range(50):
-        xnew = g(xold)
+        xnew = usarFuncion(g, xold)
         error = abs((xnew - xold) / xnew)
         if error <= tol:
             break
         else:
             xold = xnew
-    print('{:<3} {:<20} {:<25}'.format(i, xold, error))
+    return i, xold, error
+    #print('{:<3} {:<20} {:<25}'.format(i, xold, error))
 
 def metodoBiseccion(f, x0, x1, tol):
     anterior = x0
     for i in range(50):
-        if f(x0) * f(x1) < 0:
+        if usarFuncion(f, x0) * usarFuncion(f, x1) < 0:
             medio = (x0 + x1)/2
         else:
-            print('La función no tiene cero en ese intervalo')
+            return
 
         error = abs((medio - anterior) / medio)
 
         if error <= tol:
             break
 
-        if f(x0) * f(medio) < 0:
+        if usarFuncion(f, x0) * usarFuncion(f, medio) < 0:
             x1 = medio
             anterior = x1
         else:
             x0 = medio
             anterior = x0
-    print('{:<6} {:<22} {:<20}'.format(i, medio, error))    
+    return i, medio, error
+    #print('{:<6} {:<22} {:<20}'.format(i, medio, error))    
 
 def metodoFalsaPosicion(f, x0, x1, tol):
     anterior = x0
 
     for i in range(50):
-        if f(x0) * f(x1) < 0:
-            pos = x0 - (x0 - x1)*f(x0)/(f(x0) - f(x1))
+        if usarFuncion(f, x0) * usarFuncion(f, x1) < 0:
+            pos = x0 - (x0 - x1)*usarFuncion(f, x0)/(usarFuncion(f, x0) - usarFuncion(f, x1))
         error = abs((pos - anterior) / pos)
 
         if error <= tol:
             break
 
-        if f(x0) * f(pos) < 0:
+        if usarFuncion(f, x0) * usarFuncion(f, pos) < 0:
             x1 = pos
             anterior = x1
         else:
             x0 = pos
             anterior = x0
-    print('{:<3} {:<22} {:<25}'.format(i, pos, error))    
+    return i, pos, error
+    #print('{:<3} {:<22} {:<25}'.format(i, pos, error))    
 
 def metodoNewtonRaphson(f, x0, tol):
     for i in range(50):
-        x1 = x0 - ((f(x0))/(derivadaAdelante(f, x0, 0.001)))
+        x1 = x0 - ((usarFuncion(f, x0))/(derivadaAdelante(f, x0, 0.001)))
         error = abs((x1 - x0) / x1)
         if error < tol:
             break
         else:
             x0 = x1
-    print('{:<22} {:<25}'.format(x1, error))    
+    return x1, error
+    #print('{:<22} {:<25}'.format(x1, error))    
 
+def MetodoSecante(funcion, x0, x1, tol=0.00000001):
+    def der():
+        return (usarFuncion(funcion, x0) - usarFuncion(funcion, x1)) / (x0 -x1)
 
+    for i in range(50):
+        derivate = der()
+        pos = x0 - usarFuncion(funcion, x0)/derivate
+        error = abs((pos - x0) / pos)
 
+        if error <= tol:
+            break
+        else:
+            x0 = x1
+            x1 = pos
+    return i, pos, error
+    #print("{:<6} {:<22} {:<20}".format(i, pos, error))
+
+def calcularIntervalos(metodo, funcion):
+    deltaX = int(input("Ingrese valor Δ del intervalo: "))
+    limite = int(input("Ingrese valor x1 del intervalo: "))
+    raices = []
+    inferior = 0
+    superior = 0
+
+    while superior < limite:
+        
+        superior = inferior + deltaX
+        try:
+            raiz = metodo(funcion, inferior, superior, 0.0000001)
+        except:
+            raiz = None
+        if raiz != None:
+            raices.append([inferior, superior])
+        inferior = superior
+    
+    for intervalo in raices:
+        print(intervalo)
+
+    for intervalo in raices:
+            a, b = intervalo
+            print(metodo(funcion, a, b, 0.0000001))
+    
 
 
 
@@ -123,4 +166,3 @@ def verificacionFalsaPosicion():
 def verificacionNewtonRaphson():
     tol = 0.000001
     metodoNewtonRaphson(funcionExp, 1, tol)
-
