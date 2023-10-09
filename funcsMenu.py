a8849b23c224a2664bef6cb6ce84af868274ff9e
RiosVal <riosh.valeria@gmail.com>
2023-10-09 09:22:22 -0500

    agrego funcion pedir_matriz

--- funcsMenu.py
+++ funcsMenu.py
@@ -16,3 +16,35 @@ def pedirIntervalo():
 
 def usarFuncion(funcion, x):
     return eval(funcion)
+
+def pedir_matriz():
+    A = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
+    B = [0, 0, 0]
+    print("""
+          La matriz A tiene la siguiente forma:
+          [A0, A1, A2]
+          [A3, A4, A5]
+          [A6, A7, A8]
+          """)
+    A[0][0] = int(input("Ingrese A0: "))
+    A[0][1] = int(input("Ingrese A1: "))
+    A[0][2] = int(input("Ingrese A2: "))
+    A[1][0] = int(input("Ingrese A3: "))
+    A[1][1] = int(input("Ingrese A4: "))
+    A[1][2] = int(input("Ingrese A5: "))
+    A[2][0] = int(input("Ingrese A6: "))
+    A[2][1] = int(input("Ingrese A7: "))
+    A[2][2] = int(input("Ingrese A8: "))
+
+    print("""
+          La matriz B tiene la siguiente forma:
+          [B0, B1, B2]
+          """)
+
+    B[0] = int(input("Ingrese B0: "))
+    B[1] = int(input("Ingrese B1: "))
+    B[2] = int(input("Ingrese B2: "))
+
+    return A, B
